06c74916ae821239533934e523ed10f7342c896c
Dark Emrys <mathersmarshall948@gmail.com>
2023-06-26 13:34:44 +0100

    prototype complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
   const [ageMonths, setAgeMonths] = useState(0);
   const [ageYears, setAgeYears] = useState(0);
 
+  const [monthsLeft, setMonthsLeft] = useState(0);
+  const [daysLeft, setDaysLeft] = useState(0);
+
   return (
     <div
       className='font-poppins bg-white py-10 rounded-xl border-e-blue-100 px-6 max-w-3xl rounded-br-[96px]'>
@@ -44,10 +47,12 @@ function App() {
         <div className='flex justify-center border-t border-neutral-l-grey'>
           <button
             onClick={() => {
-              let { years, months, days } = age(day, month, year)
+              let { years, months, days, birthday } = age(day, month, year)
               setAgeDays(days)
               setAgeMonths(months)
               setAgeYears(years)
+              setDaysLeft(birthday.days)
+              setMonthsLeft(birthday.months)
             }}
             type='submit'
             className='flex justify-center items-center bg-primary-purple w-16 h-16 rounded-full p-4 -mt-8'>
@@ -67,6 +72,12 @@ function App() {
           <span className='text-primary-purple'>{ageDays}</span> day{ageDays == 1 ? '' : 's'}
         </p>
       </div>
+
+      <p className='text-center mt-3'>
+        Your birthday is in <br />
+        {monthsLeft} month{monthsLeft == 1 ? '' : 's'} and <br />
+        {daysLeft} day{daysLeft == 1 ? '' : 's'}.
+      </p>
 
 
     </div>
